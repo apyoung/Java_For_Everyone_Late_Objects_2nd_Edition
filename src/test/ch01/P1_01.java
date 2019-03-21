526061054859bb22506cab9c7a120ef6f9a2aa96
Andrew Young <apyoung88@gmail.com>
2019-03-21 18:47:14 -0400

    Initial commit

---
+++ src/test/ch01/P1_01.java
@@ -0,0 +1,19 @@
+package test.ch01;
+
+/**
+ * Write a program that prints a greeting of your choice,
+ * perhaps in a language other than English.
+ */
+
+public class P1_01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");  //english
+        System.out.println("Hola");  //spanish
+        System.out.println("Bonjour");  //french
+        System.out.println("Salut");  //romanian
+        System.out.println("Ciao");  //italian
+        System.out.println("Hallo");  //german
+    }
+}
